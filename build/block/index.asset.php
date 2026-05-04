06596847827a381309ee86578711fed973f4e83f
Regina Steinberg <regina.steinberg@fau.de>
2026-05-04 14:13:16 +0200

    fix: Remove node_modules from tracking

--- build/block/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'e397ca311a4fe2c20ec1');
